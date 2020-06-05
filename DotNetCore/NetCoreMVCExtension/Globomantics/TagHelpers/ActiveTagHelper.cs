c073ed950f340c4d6ad18aeb171ac29833b6561c
vintsue <vintcolor@gmail.com>
2020-06-05 16:12:44 -0500

    done - add Tag Helpers

---
+++ DotNetCore/NetCoreMVCExtension/Globomantics/TagHelpers/ActiveTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomantics.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes ="active-url")]
+    public class ActiveTagHelper: TagHelper
+    {
+        public string ActiveUrl { get; set; }
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public ActiveTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (_httpContextAccessor.HttpContext.Request.Path.ToString().Contains(ActiveUrl))
+            {
+                var existingAttrs = output.Attributes["class"]?.Value;
+                output.Attributes.SetAttribute("class", "active " + existingAttrs.ToString());
+            }
+           
+        }
+    }
+}
---
+++ DotNetCore/NetCoreMVCExtension/Globomantics/TagHelpers/CDTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Globomantics.Core.Models;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomantics.TagHelpers
+{
+    [HtmlTargetElement("cdrate")]
+    public class CDTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+        public string MeterPercent { get; set; }
+        public CDTermLength TermLength {get;set;}
+
+        private IRateService _rateService;
+
+        public CDTagHelper(IRateService rateService)
+        {
+            _rateService = rateService;
+        }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var rate = _rateService.GetCDRateByTerm(TermLength);
+            output.Content.SetHtmlContent($@"<div class=""meter"">
+                    <p> { Title }</p>
+                    <div class=""progress"">
+                        <div class=""progress-bar bg-info"" style=""width: { MeterPercent }%""> { rate }%</div>
+                    </div>
+                </div>");
+        }
+    }
+}
