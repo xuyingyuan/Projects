e153b5438d56b5bcd2cd2f1459be4eb4c5aab25f
vintsue <vintcolor@gmail.com>
2020-06-24 16:09:22 -0500

    update changes

--- DotNetCore/Authentication/Authenticatoin/Basic/Controllers/HomeController.cs
+++ DotNetCore/Authentication/Authenticatoin/Basic/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Basic.CustomAttributes;
-using Basic.CustomerPolicyProvider;
+using Basic.CustomPolicyProvider;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
